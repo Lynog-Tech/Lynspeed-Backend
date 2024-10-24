d719c2834b57d44ba61e7dd09bf9688c836d70a6
paschal-ike <paschalpaul268@gmail.com>
2024-10-24 18:38:52 +0100

    settings

--- lynspeed_project/settings.py
+++ lynspeed_project/settings.py
@@ -13,7 +13,13 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 from decouple import config
+from dotenv import load_dotenv
 
+
+
+
+# Load environment variables
+load_dotenv()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
